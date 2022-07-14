7f960aa7b0cb3927762e16a33f5d57d856dd14fc
Amitkjha28 <103565661+Amitkjha28@users.noreply.github.com>
2022-07-15 00:31:14 +0530

    Prediction

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,7 +1,9 @@
+import pickle
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+with open("covidapp/save_model/rfc_model.pkl","rb") as myfile :
+    mymodel= pickle.load(myfile)
 def myindex(request):
     # return HttpResponse('<h1> covid prediction app</h1>')
     return render(request, 'index.html')
@@ -26,5 +28,16 @@ def myresult(request):
     contactothercovid = request.POST.get('contactothercovid')
     icu = request.POST.get('icu')
     otherdisease = request.POST.get('otherdisease')
-    print(" Name : {}\n Age : {}\n Sex : {}\n patient_type :{}\n intubed :{}\n pneumonia : {}\n pregnancy : {}\n diabetes : {}\n copd : {}\n asthma : {}\n inmsupr : {}\n hypertension : {}\n other_disease : {}\n cardiovascular : {}\n obesity : {}\n renal_chronic : {}\n tobacco : {}\n contact_other_covid : {}\n icu : {}\n".format( name,age,sex,ptype,intube,pneumonia,pregnancy,diabetes,copd,asthma,inmsupr,hypertension,otherdisease,cardiovascular,obesity,renalchronic,tobacco,contactothercovid,icu))
-    return render(request,'result.html',{'name':name})
+    # print(" Name : {}\n Age : {}\n Sex : {}\n patient_type :{}\n intubed :{}\n pneumonia : {}\n pregnancy : {}\n"
+    #       " diabetes : {}\n copd : {}\n asthma : {}\n inmsupr : {}\n hypertension : {}\n other_disease : {}\n "
+    #       "cardiovascular : {}\n obesity : {}\n renal_chronic : {}\n tobacco : {}\n contact_other_covid : {}\n "
+    #       "icu : {}\n".format( name,age,sex,ptype,intube,pneumonia,pregnancy,diabetes,copd,asthma,inmsupr,
+    #                            hypertension,otherdisease,cardiovascular,obesity,renalchronic,tobacco,
+    #                            contactothercovid,icu))
+    Prediction = mymodel.predict_proba([[age, sex, ptype, intube, pneumonia, pregnancy, diabetes, copd,
+                                         asthma, inmsupr,hypertension,otherdisease, cardiovascular, obesity,
+                                         renalchronic, tobacco, contactothercovid, icu]])
+    # print("Prediction : ", Prediction)
+    result_predict =round(Prediction[[0]][0][0]*100,2)
+
+    return render(request,'result.html',{'resultpredict':result_predict,'name':name})
