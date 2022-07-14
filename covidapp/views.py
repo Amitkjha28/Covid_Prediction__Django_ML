b68601651c63500c441c9b2198aab73792d2cd18
Amitkjha28 <103565661+Amitkjha28@users.noreply.github.com>
2022-07-14 06:46:23 +0530

    template add

--- covidapp/views.py
+++ covidapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def myindex(request):
-    return HttpResponse('<h1> covid prediction app</h1>')
+    # return HttpResponse('<h1> covid prediction app</h1>')
+    return render(request, 'index.html')
